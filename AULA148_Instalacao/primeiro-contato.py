07093acd6bc1e6d2a64e644b18e314e91446c8ca
lucasfernande <lucas.fernandes6192@outlook.com>
2021-04-12 20:53:26 -0300

    adicionando um botão e uma ação de clique

--- AULA148_Instalacao/primeiro-contato.py
+++ AULA148_Instalacao/primeiro-contato.py
@@ -7,12 +7,25 @@ pip install pyqt5
 """
 
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QGridLayout
 
 
 class App(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.cw = QWidget()  # central widget
+        self.grid = QGridLayout(self.cw)
+
+        self.btn = QPushButton('Texto do botão')
+        self.btn.setStyleSheet('font-size: 21px')
+        self.grid.addWidget(self.btn, 0, 0, 1, 1)  # adicionando o botão no layout
+
+        self.btn.clicked.connect(self.falaOi)
+
+        self.setCentralWidget(self.cw)
+
+    def falaOi(self):
+        print('Olá mundo')
 
 
 if __name__ == '__main__':
